e8c55b23c4fc998d5c3d845a6cd72c2969792cd7
Aulia Ridho Verdikha <auliaridhoverdika@gmail.com>
2019-04-22 21:29:39 +0700

    first committ

--- app/src/main/java/com/arba/orilampung/HomeFragment.java
+++ app/src/main/java/com/arba/orilampung/HomeFragment.java
@@ -48,13 +48,11 @@ import com.squareup.picasso.Picasso;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class
 
 
+//test first commit
 
-
-
-HomeFragment extends Fragment {
+public class HomeFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
